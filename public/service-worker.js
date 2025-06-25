eda21a8b2c340d1ddc7d5641b6694f77ac293970
sasimf <saidhafiz1@gmail.com>
2025-06-25 15:05:43 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,12 @@
-// Service worker for PWA support
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
