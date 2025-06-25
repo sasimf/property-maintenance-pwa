bebffef7431f04ccdb0d8250ac70e208822703be
sasimf <saidhafiz1@gmail.com>
2025-06-25 15:08:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  });
+}
