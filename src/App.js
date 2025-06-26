88f0513f0b622647f5f1f222ceb6e9a1e7d58ee6
sasimf <saidhafiz1@gmail.com>
2025-06-26 16:28:09 +0100

    Delete src directory

--- src/App.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-function App() { return <div>Welcome to Property Maintenance PWA</div>; }
-export default App;
--- src/config.js
+++
@@ -1,6 +0,0 @@
-// src/config.js
-const CONFIG = {
-  API_URL: "https://your-backend-service.onrender.com"
-};
-
-export default CONFIG;
