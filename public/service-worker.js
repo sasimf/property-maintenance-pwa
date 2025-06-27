1424142b91a352f566057f6fd8c0ee2f0893cfe9
sasimf <saidhafiz1@gmail.com>
2025-06-27 20:01:34 +0100

    Delete public/service-worker.js

--- public/service-worker.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', e=>{self.skipWaiting();});
-self.addEventListener('activate', e=>{console.log('Activated');});
-self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request));});
