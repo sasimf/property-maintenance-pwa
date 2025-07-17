68a497fbaeffe188e860ce8153b4d8138718f311
sasimf <saidhafiz1@gmail.com>
2025-07-17 14:15:51 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,37 +1,30 @@
-// server/models/User.js
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   fullName:     { type: String, required: true },
-  companyName:  { type: String },                      // optional
-  address:      { type: String, required: true },
-  postcode:     { type: String, required: true },
-  phone:        { type: String, required: true },
   email:        { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
+  userType:     {
+    type: String,
+    enum: ['Landlord','Homeowner','LettingAgent','Contractor','Company'],
+    required: true
+  },
+  companyName: String,
+  address:     String,
+  postcode:    String,
+  phone:       String,
 
-  userType:     { 
-    type: String, 
-    enum: ['Landlord','Homeowner','LettingAgent','Contractor','Company'], 
-    required: true 
+  // only required for Contractor/Company
+  callOutCharge: {
+    type: Number,
+    required: function() {
+      return ['Contractor','Company'].includes(this.userType);
+    },
+    default: 0
   },
 
-  expertise:    { 
-    type: [String], 
-    default: [] 
-  },
-
-  callOutCharge: { 
-    type: Number, 
-    required: function() { 
-      return ['Contractor','Company'].includes(this.userType); 
-    }, 
-    default: 0 
-  },
-
-  createdAt:    { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
