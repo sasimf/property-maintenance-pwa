fa93191b7f9a5df236234da4bf29ecd6e53c984e
sasimf <saidhafiz1@gmail.com>
2025-07-18 02:47:09 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -17,6 +17,7 @@ async function handleResponse(res, defaultError) {
 }
 
 export async function register(data) {
+  console.log('🔗 API base URL is:', API);
   const res = await fetch(`${API}/api/users/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
